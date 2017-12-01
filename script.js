927678585a4c4cbaf9ec41b0bd499ae2d2e30639
dzeblyuk <dzeblyuk@gmail.com>
2017-12-01 15:17:32 +0200

    '#1'

---
+++ script.js
@@ -0,0 +1,20 @@
+/* function fan1() {
+  var chbox;
+  chbox = document.getElementById('one');
+
+  if (chbox.checked) {
+    alert('Выбран');
+  } else {
+    alert('Не выбран');
+  }
+}
+   */
+
+function fun1() {
+  var radio = document.getElementsByName('r1');
+  for (var i = 0; i < radio.length; i++) {
+    if (radio[i].checked) {
+      alert('Ваш ответ ' +i[value]+ ' и это кончательный ответ');
+    }
+  }
+}
